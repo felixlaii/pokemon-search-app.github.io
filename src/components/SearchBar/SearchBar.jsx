a1a594b97d56db629382919e62edab20e25b409e
Felix Lai <felix.lai@hotmail.com>
2022-11-07 11:15:23 -0500

    transfer over files

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,29 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+import { pokeAPI } from '../../utils/getPokemon';
 
-export default function SearchBar({getPokemon}) {
+export default function SearchBar() {
     const [search, setSearch] = useState('');
+    const [pokemon, setPokemon] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const getPokemon = async (query) => {
+        if(!query) {
+            setErrorMsg('You must enter a Pokemon');
+            setError(true);
+            return;
+        }
+        axios.get(`${pokeAPI}/pokemon`)
+        .then((response) => { console.log(response)
+            if(response) {
+                setPokemon(response.data.results);
+                setLoading(false);
+            }
+        })
+        .catch((error) => console.log('Pokemon not found'))
+    };
 
   return (
     <div className="search-bar">
@@ -12,7 +34,10 @@ export default function SearchBar({getPokemon}) {
           placeholder="search pokemon"
           onChange={(e) => getPokemon(e.target.value)}
         />
+        <button className="search-bar__button" onClick={(e) => getPokemon(search)}>search</button>
       </div>
+
+      
     </div>
   )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,31 +4,12 @@ import { pokeAPI } from '../../utils/getPokemon';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
 export default function HomePage() {
-    const [pokemon, setPokemon] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [errorMsg, setErrorMsg] = useState('');
 
-    const getPokemon = async (query) => {
-        if(!query) {
-            setErrorMsg('You must enter a Pokemon');
-            setError(true);
-            return;
-        }
-        axios.get(`${pokeAPI}/pokemon`)
-        .then((response) => { console.log(response)
-            if(response) {
-                setPokemon(response.data.results);
-                setLoading(false);
-            }
-        })
-        .catch((error) => console.log('Pokemon not found'))
-    };
 
 
   return (
     <div>
-        <SearchBar getPokemon={getPokemon} />
+        <SearchBar />
     </div>
   )
 }
