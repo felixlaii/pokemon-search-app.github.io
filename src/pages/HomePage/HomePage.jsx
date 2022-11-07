9d77401d2dada16d150e3cb46588ffed5b60ecf5
Felix Lai <felix.lai@hotmail.com>
2022-11-07 10:44:17 -0500

    updated aps js

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { pokeAPI } from '../../utils/getPokemon';
 
 export default function HomePage() {
     const [pokemon, setPokemon] = useState();
@@ -6,7 +8,22 @@ export default function HomePage() {
     const [error, setError] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
 
-    
+    const getPokemon = async (query) => {
+        if(!query) {
+            setErrorMsg('You must enter a Pokemon');
+            setError(true);
+            return;
+        }
+        axios.get(`${pokeAPI}/pokemon`)
+        .then((response) => { console.log(response)
+            if(response) {
+                setPokemon(response);
+                setLoading(false);
+            }
+        })
+        .catch((error) => console.log('Pokemon not found'))
+    };
+
 
   return (
     <div>HomePage</div>
